06cc1e1550910742e9b600604f65c3aea2a2e6f9
Surajbarole <surajbarole1@gmail.com>
2023-07-05 18:29:38 +0530

    Update something Calculation

--- tax_calculator.js
+++ tax_calculator.js
@@ -6,16 +6,16 @@ function findTax(salary) {
         tax = 0;
         break;
       case (salary > 500000 && salary <= 1000000):
-        tax = salary * 10 / 100;
+        tax = salary * 0.1;
         break;
       case (salary > 1000000 && salary <= 1500000):
-        tax = salary * 20 / 100;
+        tax = salary * 0.2;
         break;
       case (salary > 1500000):
-        tax = salary * 30 / 100;
+        tax = salary * 0.3;
         break;
       default:
-        throw new ValidationError("Salary not valid");
+        throw new ValidationError(" Salary is Low ");
     }
   } catch (error) {
     return error.message;
